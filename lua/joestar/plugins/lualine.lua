2b7eafad54487d687ed64539bd42a02d504a5be0
Oleh Ohnivchuk <oohnivch@c3r3p1.42vienna.com>
2024-11-06 16:56:25 +0100

    new update

--- lua/joestar/plugins/lualine.lua
+++ lua/joestar/plugins/lualine.lua
@@ -1,5 +1,8 @@
 require('lualine').setup {
   options = {
     theme = 'catppuccin',
+    -- section_separators = {'', ''},
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
   },
 }
